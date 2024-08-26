0a85bafb8c0ecfe2ad75579e17e169de65e1a64b
VatsalyaGautam <vatsalya@ssipmt.com>
2024-08-27 01:19:46 +0530

    Improved the time complexity to O(log n) by using BinarySearch instead of LinearSearch

--- 162/162_Find_Peak_Element.cpp
+++ 162/162_Find_Peak_Element.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 class Solution
@@ -9,54 +10,56 @@ public:
     {
         int n = nums.size();
         if (n == 1)
-        {
             return 0;
-        }
-        else if (nums[0] > nums[1])
-        {
+        if (nums[0] > nums[1])
             return 0;
-        }
-        else if (nums[n - 1] > nums[n - 2])
-        {
+        if (nums[n - 1] > nums[n - 2])
             return n - 1;
-        }
 
-        for (int i = 0; i < n - 1; i++)
+        int low = 1, high = n - 2, mid;
+        while (low <= high)
         {
-            if (nums[i + 1] > nums[i + 2] && nums[i + 1] > nums[i])
+            mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1])
             {
-                return i + 1;
+                return mid;
+            }
+            else if (nums[mid] < nums[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
             }
         }
         return -1;
     }
 };
 
-// Driver function to test the Solution class
 int main()
 {
     Solution solution;
 
-    vector<vector<int>> testCases = {
-        {1, 2, 3, 1},
-        {1, 2, 1, 3, 5, 6, 4},
-        {1},
-        {3, 2, 1},
-        {1, 3, 2},
-        {2, 1}};
+    // Test case 1
+    vector<int> nums1 = {1, 2, 3, 1};
+    cout << "Test case 1: Peak element index is " << solution.findPeakElement(nums1) << endl;
 
-    for (const auto &nums : testCases)
-    {
-        int peakIndex = solution.findPeakElement(const_cast<vector<int> &>(nums));
-        cout << "Peak element index for [";
-        for (size_t i = 0; i < nums.size(); i++)
-        {
-            cout << nums[i];
-            if (i != nums.size() - 1)
-                cout << ", ";
-        }
-        cout << "] is: " << peakIndex << endl;
-    }
+    // Test case 2
+    vector<int> nums2 = {1, 2, 1, 3, 5, 6, 4};
+    cout << "Test case 2: Peak element index is " << solution.findPeakElement(nums2) << endl;
+
+    // Test case 3
+    vector<int> nums3 = {1, 2};
+    cout << "Test case 3: Peak element index is " << solution.findPeakElement(nums3) << endl;
+
+    // Test case 4
+    vector<int> nums4 = {3, 4, 3, 2, 1};
+    cout << "Test case 4: Peak element index is " << solution.findPeakElement(nums4) << endl;
+
+    // Test case 5
+    vector<int> nums5 = {5, 6, 7, 8, 9};
+    cout << "Test case 5: Peak element index is " << solution.findPeakElement(nums5) << endl;
 
     return 0;
 }
