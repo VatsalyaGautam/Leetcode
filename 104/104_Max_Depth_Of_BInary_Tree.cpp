a0baa5948ebc219b777cb449b11afd189f7c53e2
VatsalyaGautam <vatsalya@ssipmt.com>
2025-03-02 17:21:49 +0530

    solved another trees question

---
+++ 104/104_Max_Depth_Of_BInary_Tree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return max(1 + maxDepth(root->left), 1 + maxDepth(root->right));
+    }
+};
+
+// Driver program to test the solution
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    
+    Solution sol;
+    cout << sol.maxDepth(root) << endl;
+    
+    return 0;
+}
